86952df633a0cf754242e4c5d530cfb1319ebe3b
ACole75 <a.cole7@my.bpp.com>
2026-02-06 11:52:06 +0000

    added new py file

---
+++ Demo_1.py
@@ -0,0 +1,7 @@
+# hello.py
+import os
+print("Program starting...")
+print("Running on:", os.name)
+print("Process ID:", os.getpid())
+print("Working directory:", os.getcwd())
+print("Program complete.")
